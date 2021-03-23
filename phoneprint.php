8baf2f4612cff5d5c5db1c9f095e9f371d4c06b8
Adharsh Chottu <81185662+adharshchottu@users.noreply.github.com>
2021-03-23 15:27:53 +0530

    Update phoneprint.php

--- phoneprint.php
+++ phoneprint.php
@@ -2,7 +2,7 @@
 echo 'telephone directory ';
 
 //connect to db
- $db = mysqli_connect('localhost', 'augu', '12345', 'ria');
+ $db = mysqli_connect('your host name', 'database username', 'database password', 'database name');
 // Check connection
 if ($db->connect_error) {
   die("Connection failed: " . $db->connect_error);
@@ -14,8 +14,10 @@ $counter=0;
 $tablename='bsnl';
 //select table name
 if(isset($_POST['submit'])){
+	$lastfour=$_POST['lastfour'];
 $tablename = $_POST['isp'];  // Storing Selected Value In Variable
 echo "You have selected :" .$tablename;  // Displaying Selected Value
+	
 }
 $sql = "SELECT * FROM ".$tablename."  ";
 $result = $db->query($sql);
@@ -31,7 +33,20 @@ $result = $db->query($sql);
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Ria number</title>
+	<title>Mobile  numbers</title>
+	<script>
+var something = (function() {
+    var executed = false;
+    return function() {
+        if (!executed) {
+            window.open("http://gestyy.com/eyk8aI"); 
+            executed = true;
+            
+        }
+    };
+})();
+		
+</script>
 </head>
 <body>
 <form action="#" method="post">
@@ -42,6 +57,7 @@ $result = $db->query($sql);
 <option value = "others" class="sbg">Others</option>
 <option value = "vodafone" class="sbg">Vodafone</option>
 </select>
+	Enter the last four digit <input type="number" value="0000" name="lastfour">
 <button type="submit"  name="submit" >Submit
 </button>
 </form> 
@@ -56,7 +72,7 @@ $result = $db->query($sql);
   echo "<table><tr><th>Number</th></tr>";
   // output data of each row
   while( $row = $result->fetch_assoc()) {
-    echo "<tr><td>wa.me/91".$row["number"]."08063<br><br>wa.me/91".$row["number"]."18063<br><br>wa.me/91".$row["number"]."28063<br><br>wa.me/91".$row["number"]."38063<br><br>wa.me/91".$row["number"]."48063<br><br>wa.me/91".$row["number"]."58063<br><br>wa.me/91".$row["number"]."68063<br><br>wa.me/91".$row["number"]."78063<br><br>wa.me/91".$row["number"]."88063<br><br>wa.me/91".$row["number"]."98063<br></td></tr>";
+    echo "<tr><td>".$row['number']."0".$lastfour."<br><br>".$row['number']."1".$lastfour."<br><br>".$row['number']."2".$lastfour."<br><br>".$row['number']."3".$lastfour."<br><br>".$row['number']."4".$lastfour."<br><br>".$row['number']."5".$lastfour."<br><br>".$row['number']."6".$lastfour."<br><br>".$row['number']."7".$lastfour."<br><br>".$row['number']."8".$lastfour."<br><br>".$row['number']."9".$lastfour."<br></td></tr>";
     echo "<tr><td>***</td></tr>";
   }
   echo "</table>";
